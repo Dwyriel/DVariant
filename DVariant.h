ad411a02a6dc870a813dabb691cb22038a36b3d0
Dwyriel <dwyriel333@hotmail.com>
2023-08-24 10:49:04 -0300

    Header only and templated object
    Only performs shallow copy.

--- DVariant.h
+++ DVariant.h
@@ -2,74 +2,235 @@
 #define DVARIANT_H
 
 #include <string>
+#include <cstring>
 
 class DVariant {
     enum class Type : u_int8_t {
         Object, String, FloatingPoint, Integer, Boolean
     };
 
+    static const size_t defaultSize = 9;
+
     Type m_type;
     void *m_data;
     size_t m_size;
 
-    void modifyData(const void *from, size_t size, DVariant::Type type);
+    inline void modifyData(const void *from, size_t size, DVariant::Type type) {
+        memset(m_data, 0, defaultSize);
+        memcpy(m_data, from, size);
+        m_type = type;
+    }
 
 public:
     using Type = Type;
 
-    DVariant() noexcept;
+    DVariant() noexcept: m_type(Type::String) {
+        m_size = defaultSize;
+        m_data = calloc(m_size, 1);
+    }
 
-    DVariant(void *value, size_t size) noexcept;
+    /**
+     * @warning only performs a shallow copy, do not use for complex objects.
+     */
+    template<class T>
+    DVariant(const T &value) noexcept: m_type(Type::Object) {
+        auto size = sizeof(T);
+        m_size = size > defaultSize ? size : defaultSize;
+        m_data = malloc(m_size);
+        memcpy(m_data, &value, size);
+    }
 
-    DVariant(const char *value) noexcept;
+    DVariant(void *value, size_t size) noexcept: m_type(Type::Object) {
+        m_size = size > defaultSize ? size : defaultSize;
+        m_data = malloc(m_size);
+        memcpy(m_data, value, size);
+    }
 
-    DVariant(const std::string &value) noexcept;
+    DVariant(const char *value) noexcept: m_type(Type::String) {
+        m_size = strlen(value) + 1;
+        m_data = malloc(m_size);
+        memcpy(m_data, value, m_size);
+    }
 
-    DVariant(double value) noexcept;
+    DVariant(const std::string &value) noexcept: m_type(Type::String) {
+        m_size = value.size() + 1;
+        m_data = malloc(m_size);
+        memcpy(m_data, value.c_str(), m_size);
+    }
 
-    DVariant(int64_t value) noexcept;
+    DVariant(double value) noexcept: m_type(Type::FloatingPoint) {
+        m_size = defaultSize;
+        m_data = calloc(m_size, 1);
+        memcpy(m_data, &value, sizeof(value));
+    }
 
-    DVariant(int32_t value) noexcept;
+    DVariant(int64_t value) noexcept: m_type(Type::Integer) {
+        m_size = defaultSize;
+        m_data = calloc(m_size, 1);
+        memcpy(m_data, &value, sizeof(value));
+    }
 
-    DVariant(bool value) noexcept;
+    DVariant(int32_t value) noexcept: m_type(Type::Integer) {
+        m_size = defaultSize;
+        m_data = calloc(m_size, 1);
+        memcpy(m_data, &value, sizeof(value));
+    }
 
-    DVariant(const DVariant &dVariant) noexcept;
+    DVariant(bool value) noexcept: m_type(Type::Boolean) {
+        m_size = defaultSize;
+        m_data = calloc(m_size, 1);
+        memcpy(m_data, &value, sizeof(value));
+    }
 
-    DVariant(DVariant &&dVariant) noexcept;
+    DVariant(const DVariant &dVariant) noexcept {
+        m_type = dVariant.m_type;
+        m_size = m_type == Type::String ? dVariant.m_size : defaultSize;
+        m_data = malloc(m_size);
+        memcpy(m_data, dVariant.m_data, m_size);
+    }
 
-    ~DVariant();
+    DVariant(DVariant &&dVariant) noexcept {
+        m_type = dVariant.m_type;
+        m_size = dVariant.m_size;
+        m_data = dVariant.m_data;
+        dVariant.m_data = nullptr;
+    }
 
-    void CustomObject(void *value, size_t size) noexcept;
+    ~DVariant() {
+        free(m_data);
+    }
 
-    void *AsCustom() const noexcept;
+    /**
+     * @warning only performs a shallow copy, do not use for complex objects.
+     */
+    template<class T>
+    void SetObject(const T &value) {
+        auto size = sizeof(T);
+        if (size > m_size) {
+            m_data = realloc(m_data, size);
+            m_size = size;
+        }
+        memcpy(m_data, &value, size);
+        m_type = Type::Object;
+    }
 
-    const char *AsCString() const noexcept;
+    /**
+     * @warning only performs a shallow copy, do not use for complex objects.
+     */
+    void SetObject(void *value, size_t size) noexcept {
+        if (size > m_size) {
+            m_data = realloc(m_data, size);
+            m_size = size;
+        }
+        memcpy(m_data, value, size);
+        m_type = Type::Object;
+    }
 
-    std::string AsString() const noexcept;
+    template<class T>
+    T AsObject() {
+        return *(T *) m_data;
+    }
 
-    double AsDouble() const noexcept;
+    void *AsCustom() const noexcept {
+        return m_data;
+    }
 
-    int64_t AsInteger() const noexcept;
+    const char *AsCString() const noexcept {
+        return (char *) m_data;
+    }
 
-    bool AsBool() const noexcept;
+    std::string AsString() const noexcept {
+        std::string str = (const char *) m_data;
+        return str;
+    }
 
-    Type GetType() const;
+    double AsDouble() const noexcept {
+        return *((double *) m_data);
+    }
 
-    DVariant &operator=(const char *value) noexcept;
+    int64_t AsInteger() const noexcept {
+        return *((int64_t *) m_data);
+    }
 
-    DVariant &operator=(const std::string &value) noexcept;
+    bool AsBool() const noexcept {
+        return *((bool *) m_data);
+    }
 
-    DVariant &operator=(double value) noexcept;
+    Type GetType() const {
+        return m_type;
+    }
 
-    DVariant &operator=(int64_t value) noexcept;
+    /**
+     * @warning only performs a shallow copy, do not use for complex objects.
+     */
+    template<class T>
+    DVariant &operator=(const T &value) {
+        auto size = sizeof(T);
+        if (size > m_size) {
+            m_data = realloc(m_data, size);
+            m_size = size;
+        }
+        memcpy(m_data, &value, size);
+        m_type = Type::Object;
+        return *this;
+    }
 
-    DVariant &operator=(int32_t value) noexcept;
+    DVariant &operator=(const char *value) noexcept {
+        size_t strSize = strlen(value) + 1;
+        if (strSize > m_size) {
+            m_data = realloc(m_data, strSize);
+            m_size = strSize;
+        }
+        memcpy(m_data, value, strSize);
+        m_type = Type::String;
+        return *this;
+    }
 
-    DVariant &operator=(bool value) noexcept;
+    DVariant &operator=(const std::string &value) noexcept {
+        operator=(value.c_str());
+        return *this;
+    }
 
-    DVariant &operator=(const DVariant &dVariant) noexcept;
+    DVariant &operator=(double value) noexcept {
+        modifyData(&value, sizeof(value), Type::FloatingPoint);
+        return *this;
+    }
 
-    DVariant &operator=(DVariant &&dVariant) noexcept;
+    DVariant &operator=(int64_t value) noexcept {
+        modifyData(&value, sizeof(value), Type::Integer);
+        return *this;
+    }
+
+    DVariant &operator=(int32_t value) noexcept {
+        modifyData(&value, sizeof(value), Type::Integer);
+        return *this;
+    }
+
+    DVariant &operator=(bool value) noexcept {
+        modifyData(&value, sizeof(value), Type::Boolean);
+        return *this;
+    }
+
+    DVariant &operator=(const DVariant &dVariant) noexcept {
+        if (this == &dVariant)
+            return *this;
+        if (dVariant.m_type == Type::String && dVariant.m_size > m_size) {
+            m_data = realloc(m_data, dVariant.m_size);
+            m_size = dVariant.m_size;
+        }
+        memcpy(m_data, dVariant.m_data, dVariant.m_size);//check copy constructor
+        m_type = dVariant.m_type;
+        return *this;
+    }
+
+    DVariant &operator=(DVariant &&dVariant) noexcept {
+        free(m_data);
+        m_type = dVariant.m_type;
+        m_size = dVariant.m_size;
+        m_data = dVariant.m_data;
+        dVariant.m_data = nullptr;
+        return *this;
+    }
 };
 
 #endif //DVARIANT_H
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # DVariant
 
- A simple yet decently performant variant implementation for basic types like integers, floating points and C strings.
+ A simple yet decently performant variant implementation for basic types like integers, floating points, C strings and non-complex objects (like C structs).
